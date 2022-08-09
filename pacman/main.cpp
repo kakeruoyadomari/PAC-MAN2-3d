5e8df0126692b90dc387b609ec768c28fb4a5123
Mitsuki-2-3 <107020749+Mitsuki-2-3@users.noreply.github.com>
2022-08-09 11:48:45 +0900

    画面更新の追加

--- pacman/main.cpp
+++ pacman/main.cpp
@@ -12,11 +12,16 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 		return -1;			// エラーが起きたら直ちに終了
 	}
 
+	SetDrawScreen(DX_SCREEN_BACK);
+
 	SceneManager scene(new Title());
 
 	while (scene.Update(scene.ControllerUpdate()) != nullptr)
 	{
+		ClearDrawScreen();		//画面の初期化
 		scene.Draw();
+		ScreenFlip();				//裏画面の内容を表画面に反映
+
 	}
 
 
