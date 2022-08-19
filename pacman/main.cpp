9b7e9b782edd959a27df32472bdaff1df5bae567
Mitsuki-2-3 <107020749+Mitsuki-2-3@users.noreply.github.com>
2022-08-19 13:14:54 +0900

    kousin

--- pacman/main.cpp
+++ pacman/main.cpp
@@ -4,8 +4,10 @@
 
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
 {
-	ChangeWindowMode(TRUE);
-	SetGraphMode(1280, 720, 32);
+	SetGraphMode(1280, 720, 16);
+	SetFullScreenResolutionMode(DX_FSRESOLUTIONMODE_DESKTOP);
+	//SetFullScreenResolutionMode(DX_FSRESOLUTIONMODE_MAXIMUM);  
+
 
 	if (DxLib_Init() == -1)		// ‚c‚wƒ‰ƒCƒuƒ‰ƒŠ‰Šú‰»ˆ—
 	{
